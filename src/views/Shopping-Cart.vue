f98441a280987dfd5936c86c094f2f8a89367866
phuong <phuong@bespokify.com>
2021-11-02 17:11:02 +0700

    # 2-11 Update section service of ShoppingCart

--- src/views/Shopping-Cart.vue
+++ src/views/Shopping-Cart.vue
@@ -1,6 +1,82 @@
 <template>
   <div>
     <top-nav-general />
+    <section id="service" class="py-9">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6 col-lg-3">
+            <!-- Item -->
+            <div class="d-flex mb-6 mb-lg-0">
+              <!-- Icon -->
+              <i class="fa fa-truck font-size-lg text-primary"></i>
+
+              <!-- Body -->
+              <div class="ml-6">
+                <!-- Heading -->
+                <h6 class="heading-xxs mb-1">Free shipping</h6>
+
+                <!-- Text -->
+                <p class="mb-0 font-size-sm text-muted">
+                  From all orders over $100
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <!-- Item -->
+            <div class="d-flex mb-6 mb-lg-0">
+              <!-- Icon -->
+              <i class="fa fa-exchange-alt font-size-lg text-primary"></i>
+
+              <!-- Body -->
+              <div class="ml-6">
+                <!-- Heading -->
+                <h6 class="mb-1 heading-xxs">Free returns</h6>
+
+                <!-- Text -->
+                <p class="mb-0 font-size-sm text-muted">
+                  Return money within 30 days
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <!-- Item -->
+            <div class="d-flex mb-6 mb-md-0">
+              <!-- Icon -->
+              <i class="fa fa-lock font-size-lg text-primary"></i>
+
+              <!-- Body -->
+              <div class="ml-6">
+                <!-- Heading -->
+                <h6 class="mb-1 heading-xxs">Secure shopping</h6>
+
+                <!-- Text -->
+                <p class="mb-0 font-size-sm text-muted">You're in safe hands</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <!-- Item -->
+            <div class="d-flex">
+              <!-- Icon -->
+              <i class="fa fa-tag font-size-lg text-primary"></i>
+
+              <!-- Body -->
+              <div class="ml-6">
+                <!-- Heading -->
+                <h6 class="mb-1 heading-xxs">Over 10,000 Styles</h6>
+
+                <!-- Text -->
+                <p class="mb-0 font-size-sm text-muted">
+                  We have everything you need
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <bottom-navigation />
   </div>
 </template>
